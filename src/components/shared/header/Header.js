1e14c817577de7505a8fa4767cba09af51ed79a4
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-10 12:26:11 +0200

    Adds header comp and bootstrap css and js

---
+++ src/components/shared/header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Header() {
+
+  return (
+    <h1> I am Header Component</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
