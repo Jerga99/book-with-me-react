853465f33f730a2b0580d336fd9b34d401b3e3f2
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-11 13:08:05 +0200

    Changes rental details structure

---
+++ src/components/rental/rental-detail/RentalDetailInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { RentalAssets } from './RentalAssets';
+
+export function RentalDetailInfo({rental}) {
+  return (
+    <div className="rental">
+      <h2 className="rental-type ${rental.category}">{rental.shared} {rental.category}</h2>
+      <h1 className="rental-title">{rental.title}</h1>
+      <h2 className="rental-city">{rental.city}</h2>
+      <div className="rental-room-info">
+        <span><i className="fa fa-building"></i>{rental.bedrooms} bedrooms</span>
+        <span><i className="fa fa-user"></i> {rental.bedrooms + 4} guests</span>
+        <span><i className="fa fa-bed"></i> {rental.bedrooms + 2} beds</span>
+      </div>
+      <p className="rental-description">
+        {rental.description}
+      </p>
+      <hr></hr>
+      <RentalAssets></RentalAssets>
+    </div>
+  )
+}
