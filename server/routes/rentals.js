680ebe1ce99b8a15dd1c94b1c484b4eead512b2f
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-15 02:52:27 +0200

    Adds booking creation

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const seedDb = require("../seed-db");
 const Rental = require("../models/rental");
+const User = require("../models/user");
+const Auth = require("../controllers/auth");
+const {normalizeErrors} = require("../helpers/mongoose-helper");
 
+// TEST WITH AUTH MIDDLEWARE
 router.get("", function(req, res) {
   const city = req.query.city;
 
   if (city) {
-    Rental.find({city: city.toLowerCase()}, function(err, filteredRentals) {
+    Rental.find({city: city.toLowerCase()}).select('-bookings').exec(function(err, filteredRentals) {
       if (err || filteredRentals.length === 0 ) {
         return res.status(422).send({errors: [{title: 'No Rentals found', detail: `There are no rentals for city ${city}`}] });
       }
@@ -15,16 +18,20 @@ router.get("", function(req, res) {
       res.json(filteredRentals);
     });
   } else {
-      Rental.find({}, function(err, allRentals) {
+      Rental.find({}).select('-bookings').exec(function(err, allRentals) {
       res.json(allRentals);
     });
   }
 });
 
 router.get("/:id", function(req, res) {
-  Rental.findById(req.params.id, function(err, foundRental) {
-    res.json(foundRental);
+  Rental.findById(req.params.id).
+    populate('user', 'email -_id').
+    populate('bookings', 'startAt endAt -_id').
+    exec(function(err, foundRental) {
+      res.json(foundRental);
   });
 });
 
 module.exports = router;
+
