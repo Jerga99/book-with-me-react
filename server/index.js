0dee4c28177c0503f15849cc0f99f15c47559c25
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-18 13:05:15 +0200

    Removes prod check

--- server/index.js
+++ server/index.js
@@ -24,14 +24,14 @@ app.use("/api/v1/rentals", rentalsRoutes);
 app.use("/api/v1/bookings", bookingRoutes);
 
 
-if (process.env.NODE_ENV == 'production') {
-  const appPath = path.join(__dirname, "..", "build");
-  app.use(express.static(appPath));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.resolve(appPath, 'index.html'));
-  });
-}
+const appPath = path.join(__dirname, "..", "build");
+app.use(express.static(appPath));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(appPath, 'index.html'));
+});
+
 
 const PORT = process.env.PORT || '3001';
 
