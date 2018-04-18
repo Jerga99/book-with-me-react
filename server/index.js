4f54864ef28d9df114de54352a599ffc002e2ac4
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-18 15:09:09 +0200

    Adds DB uri

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const rentalsRoutes = require("./routes/rentals"),
       bookingRoutes = require("./routes/bookings"),
       authRoutes    = require("./routes/auth");
 
-const url = `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@ds241489.mlab.com:41489/bwm-dev-react`;
+const url = `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@${DB_URI}`;
 
 mongoose.connect(url).then(() => {
   if (process.env.NODE_ENV != 'production') {fakeDB.seed();}
