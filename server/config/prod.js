fe34e3b44640816e82ec43fe322abeb81e6c06a4
Filip Jerga <filipjerga@filips-mbp.lan>
2018-04-18 13:45:00 +0200

    Server key setup

---
+++ server/config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DB_USER: process.ENV.DB_USER,
+  DB_PASSWORD: process.ENV.DB_PASSWORD,
+  SECRET: process.ENV.SECRET
+}
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express       = require("express"),
       app           = express(),
       bodyParser    = require("body-parser"),
       mongoose      = require("mongoose"),
-      keys          = require("./keys"),
+      config        = require("./config"),
       fakeDB        = require("./seed-db"),
       Rental        = require("./models/rental"),
       path          = require("path");
@@ -11,7 +11,7 @@ const rentalsRoutes = require("./routes/rentals"),
       bookingRoutes = require("./routes/bookings"),
       authRoutes    = require("./routes/auth");
 
-const url = `mongodb://${keys.DB_USER}:${keys.DB_PASSWORD}@ds241489.mlab.com:41489/bwm-dev-react`;
+const url = `mongodb://${config.DB_USER}:${config.DB_PASSWORD}@ds241489.mlab.com:41489/bwm-dev-react`;
 
 mongoose.connect(url).then(() => {
   if (process.env.NODE_ENV != 'production') {fakeDB.seed();}
@@ -24,14 +24,14 @@ app.use("/api/v1/rentals", rentalsRoutes);
 app.use("/api/v1/bookings", bookingRoutes);
 
 
+if (process.env.NODE_ENV == 'production') {
+  const appPath = path.join(__dirname, "..", "build");
+  app.use(express.static(appPath));
 
-const appPath = path.join(__dirname, "..", "build");
-app.use(express.static(appPath));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(appPath, 'index.html'));
-});
-
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || '3001';
 
